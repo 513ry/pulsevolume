ae790053f956a1826125b4c51ffe50770cf382f5
siery <siery@comic.com>
2024-11-01 08:09:18 +0100

    Sync with i3bar cycle to avoid worse case scenario double time

--- pulsevolume.sh
+++ pulsevolume.sh
@@ -5,6 +5,14 @@ CACHE_DIR=~/.cache/pulsevolume
 # Crate cache directory if needed
 mkdir -p $CACHE_DIR
 
+# Calculate how many seconds until next point divisible by 5
+declare -ir sec=$(date +%S)
+wait_time=$((5 - sec % 5))
+wait_ms=$((wait_time * 1000 - 20))
+if ((wait_ms > 0)); then
+  sleep $(echo "scale=3; $wait_ms / 1000" | bc)
+fi
+
 while true; do
   pa_default_sink=$(pactl get-default-sink)
   [[ $(pactl get-sink-mute $pa_default_sink) = "Mute: yes" ]] && declare -l pa_sink_mute=true || declare -l pa_sink_mute=false
